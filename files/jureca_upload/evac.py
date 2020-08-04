f9654d753a03c1ed825609dc7e74e733bfe2e1eb
Jonas Rzezonka <jonas.rzez@web.de>
2020-08-04 16:30:49 +0200

    commit to change branch

--- files/jureca_upload/evac.py
+++ files/jureca_upload/evac.py
@@ -132,7 +132,7 @@ def var_ini():
     i_start = 0
     i_end = 5
     N_runs = i_end
-    t_max = 103
+    t_max = 105
     periodic = 0
     return rho_ini, T, v0, esigma, fps, stepsize, N_ped, i_start, i_end, t_max, periodic, r, N_runs, rho_min, rho_max, avoid_wall, mot_frac
 
